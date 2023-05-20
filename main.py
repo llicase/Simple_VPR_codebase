7473329cbb773e1fc7f777990a51d08c9cf5ebe4
llicase <131463355+llicase@users.noreply.github.com>
2023-05-20 15:41:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,21 +20,49 @@ from datasets.test_dataset import TestDataset
 from datasets.train_dataset import TrainDataset
 
 
+class GeM(torch.nn.Module):
+    def __init__(self, p=2.5, eps=1e-6):
+        super(GeM,self).__init__()
+        self.p = torch.nn.Parameter(torch.ones(1)*p)
+        self.eps = eps
+    
+    def forward(self, x):
+        return torch.nn.functional.avg_pool2d(x.clamp(min=self.eps).pow(self.p), (x.size(-2), x.size(-1))).pow(1./self.p)
+
 class LightningModel(pl.LightningModule):
-    def __init__(self, val_dataset, test_dataset, descriptors_dim=512, num_preds_to_save=0, save_only_wrong_preds=True, marg=28.6, sc=64):
+    def __init__(self, val_dataset, test_dataset, descriptors_dim=512, num_preds_to_save=0, save_only_wrong_preds=True, 
+                 alpha_param=1, beta_param=50, base_param=0.0, eps_param=0.1, opt_param="sgd", loss_param="cl", 
+                 pool_param="None", miner_param="None", lr_adam_param=0.0001):
         super().__init__()
         self.val_dataset = val_dataset
         self.test_dataset = test_dataset
         self.num_preds_to_save = num_preds_to_save
         self.save_only_wrong_preds = save_only_wrong_preds
+        self.opt_param = opt_param
+        self.loss_param = loss_param
+        self.pool_param = pool_param
+        self.miner_param = miner_param
+        self.lr_adam_param = lr_adam_param
+        
+        #set the miner
+        if self.miner_param == "ms":
+            self.miner = miners.MultiSimilarityMiner(epsilon=eps_param, distance=CosineSimilarity())
+        
         # Use a pretrained model
         self.model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.DEFAULT)
+        
+        #set the kind of pooling
+        if self.pool_param == "gem":
+            self.model.avgpool= GeM()
+        
         # Change the output of the FC layer to the desired descriptors dimension
         self.model.fc = torch.nn.Linear(self.model.fc.in_features, descriptors_dim)
+        
         # Set the loss function
-        self.loss_fn = losses.ArcFaceLoss(num_classes=51600, embedding_size=512)
-        # List of the parameters to pass to the optimizer
-        self.arcface_params = list(self.loss_fn.parameters())
+        if self.loss_param == "cl":
+            self.loss_fn = losses.ContrastiveLoss(pos_margin=0, neg_margin=1)
+        elif self.loss_param == "ms":
+            self.loss_fn = losses.MultiSimilarityLoss(alpha=alpha_param, beta=beta_param, base=base_param)
 
 
     def forward(self, images):
@@ -42,17 +70,25 @@ class LightningModel(pl.LightningModule):
         return descriptors
 
     def configure_optimizers(self):
-        model_optimizer = torch.optim.SGD(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
-        loss_optimizer = torch.optim.SGD(self.arcface_params, lr=0.01)
-        return [model_optimizer, loss_optimizer]
+        if self.opt_param == "sgd":
+            optimizers = torch.optim.SGD(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
+        elif self.opt_param == "adam":
+            optimizers = torch.optim.Adam(self.parameters(), lr=self.lr_adam_param, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+        elif self.opt_param == "adamw":
+            optimizers = torch.optim.AdamW(self.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01)
+        elif self.opt_param == "adamax":
+            optimizers = torch.optim.Adamax(self.parameters(), lr=0.002, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+        elif self.opt_param == "amsgrad":
+            optimizers = torch.optim.Adam(self.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=True)
+        return optimizers
 
     #  The loss function call (this method will be called at each training iteration)
-    def loss_function(self, descriptors, labels):
-        loss = self.loss_fn(descriptors, labels)
+    def loss_function(self, descriptors, labels, triplets):
+        loss = self.loss_fn(descriptors, labels, triplets)
         return loss
 
     # This is the training step that's executed at each iteration
-    def training_step(self, batch, batch_idx, optimizer_idx):
+    def training_step(self, batch, batch_idx):
         images, labels = batch
         num_places, num_images_per_place, C, H, W = images.shape
         images = images.view(num_places * num_images_per_place, C, H, W)
@@ -60,6 +96,7 @@ class LightningModel(pl.LightningModule):
 
         # Feed forward the batch to the model
         descriptors = self(images)  # Here we are calling the method forward that we defined above
+        triplets = self.miner(descriptors, labels)
         loss = self.loss_function(descriptors, labels)  # Call the loss_function we defined above
         
         self.log('loss', loss.item(), logger=True)
@@ -127,7 +164,9 @@ if __name__ == '__main__':
 
     train_dataset, val_dataset, test_dataset, train_loader, val_loader, test_loader = get_datasets_and_dataloaders(args)
     
-    model = LightningModel(val_dataset, test_dataset, args.descriptors_dim, args.num_preds_to_save, args.save_only_wrong_preds, args.margin, args.scale)
+    model = LightningModel(val_dataset, test_dataset, args.descriptors_dim, args.num_preds_to_save, args.save_only_wrong_preds, 
+                           alpha_param=args.alpha, beta_param=args.beta, base_param=args.base, eps_param=args.eps, opt_param=args.opt, 
+                           loss_param=args.loss, pool_param=args.pool, miner_param=args.miner, lr_adam_param=args.lr_adam)
     
     # Model params saving using Pytorch Lightning. Save the best 3 models according to Recall@1
     checkpoint_cb = ModelCheckpoint(
